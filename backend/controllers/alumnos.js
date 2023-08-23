f85cc08c050c13078d63c4379cb678e2da4a15ca
Jesus Barajas <jebarfig21@ciencias.unam.mx>
2023-08-22 18:35:22 -0600

    Borrado de Carpetas, Creacion de create basico

--- backend/controllers/alumnos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const alumnosController = require('../controllers/alumnosController');
-
-// Ruta para obtener todos los alumnos
-router.get('/alumnos', alumnosController.obtenerAlumnos);
-router.post('/alumnos', alumnosController.crearAlumno);
-
-module.exports = router;
